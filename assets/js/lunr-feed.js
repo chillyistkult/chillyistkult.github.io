af83b12b380eb2dcfda31ab631cc3e1d8ba1ebe0
Benjamin Schuch <bschuch@gmx.net>
2017-04-06 01:25:36 +0200

    Translation

--- assets/js/lunr-feed.js
+++ assets/js/lunr-feed.js
@@ -25,7 +25,7 @@ var store = [{% for post in site.posts %}{
     "title": {{post.title | jsonify}},
     "link": {{ post.url | jsonify }},
     "image": {{ post.image | jsonify }},
-    "date": {{ post.date | date: '%B %-d, %Y' | jsonify }},
+    "date": {{ post.date | date: "%d.%m.%Y" | jsonify }},
     "category": {{ post.category | jsonify }},
     "excerpt": {{ post.content | strip_html | truncatewords: 20 | jsonify }}
 }{% unless forloop.last %},{% endunless %}{% endfor %}]
@@ -38,7 +38,7 @@ $(document).ready(function() {
         var query = $(this).val();
         var result = index.search(query);
         resultdiv.empty();
-        $('.show-results-count').text(result.length + ' Results');
+        $('.show-results-count').text(result.length + ' Ergebnisse');
         for (var item in result) {
             var ref = result[item].ref;
             var searchitem = '<li><a href="'+ hostname + store[ref].link+'">'+store[ref].title+'</a></li>';
